5cf86c304b83a8605b2cc840156bb0366ceaaf63
Vincent Mahnke <github@vincent.mahn.ke>
2025-12-05 02:58:09 +0000

    fix: Removes redudnant `.`

--- infrastructure/subdomain.tf
+++ infrastructure/subdomain.tf
@@ -10,6 +10,6 @@ locals {
 resource "allinkl_dns" "subdomain" {
   zone_host   = "mahn.ke"
   record_type = "A"
-  record_name = format("%s.", local.subdomain_label)
+  record_name = local.subdomain_label
   record_data = "88.99.215.101"
 }
