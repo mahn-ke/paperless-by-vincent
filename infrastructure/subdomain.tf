83ec4fe7e3983e263f703b025ad7ec100118f3d8
Vincent Mahnke <github@vincent.mahn.ke>
2025-12-05 02:50:51 +0000

    feat: Bumps allinkl provider and replaces DDNS with DNS

--- infrastructure/subdomain.tf
+++ infrastructure/subdomain.tf
@@ -7,10 +7,9 @@ locals {
   subdomain_label = replace(replace(basename(dirname(path.cwd)), "-by-vincent", ""), "-", ".")
 }
 
-resource "allinkl_ddns" "subdomain" {
-  dyndns_comment   = replace(local.subdomain_label, ".", "_")
-  dyndns_password  = random_password.ddns_password.result
-  dyndns_zone      = "mahn.ke"
-  dyndns_label     = "${local.subdomain_label}.by.vincent"
-  dyndns_target_ip = "88.99.215.101"
+resource "allinkl_dns" "subdomain" {
+  zone_host   = "mahn.ke"
+  record_type = "A"
+  record_name = replace(local.subdomain_label, ".", "_") + "."
+  record_data = "88.99.215.101"
 }
